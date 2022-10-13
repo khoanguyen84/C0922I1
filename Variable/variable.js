04417c73874b1d8f2c1f554799a62f99bac5538a
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-10-13 10:12:30 +0700

    variable

---
+++ Variable/variable.js
@@ -0,0 +1,29 @@
+let a = 5;
+let b = 10;
+let c = 4;
+// ax2 + bx + c = 0;
+// 5x2 + 10x + 4 = 0;
+// console.log(a + "x2 + " + b + "x + " + c + " = 0");
+// console.log(`${a}x2 + ${b}x + ${c} = 0`);
+
+// let name = "Khoa";
+// "Still breath, still alive";
+
+// Khoa đã nói rằng: "Still breath, still alive"
+
+// console.log(name + ' đã nói rằng: "Still breath, still alive"' )
+// console.log(`${name} đã nói rằng: "Still breath, still alive", '1 + 1'`)
+
+
+let score = 10;
+
+//1. tăng score lên 1 đơn vị
+//2. hiển thị giá trị của score ra console
+
+// console.log(++score);
+// console.log(score++);
+// score+=1;
+// console.log(score)
+// console.log(++score + 5);
+console.log(score++ * 5);
+console.log(score)
